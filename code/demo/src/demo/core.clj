1ffbfd217d1171a89b6976bceb315a6fa4475fec
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2015-02-20 19:41:59 -0500

    TODO make this code work

--- code/demo/src/demo/core.clj
+++ code/demo/src/demo/core.clj
@@ -1,7 +1,28 @@
 (ns demo.core
   (:require [clojure.core.typed :as t]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Simple let-aliasing
+(t/fn [x :- (t/U nil t/Num)]
+  (let [x1 x]
+    (when (number? x)
+      (inc x1))))
+
+;; let-aliasing with paths
+(t/fn [{x :a :as m} :- '{:a (t/U nil t/Num)}]
+  (let [x1 x]
+    (when (number? (:a m))
+      (inc x1))))
+
+;; HMap merging
+(t/fn [m1 :- '{:a t/Num}
+       m2 :- '{:b t/Num}]
+  :- '{:a t/Num :b t/Num}
+  (merge m1 m2))
+
+;; HMap merging
+((t/fn [m1 :- '{:a t/Num}
+        m2 :- '{:b t/Num}]
+   :- '{:a t/Num :b t/Num}
+   (merge m1 m2))
+ {:a 1}
+ {:a nil :b 2})
