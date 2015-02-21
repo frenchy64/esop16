768e4a4a47b932b0936df47e9a82dc441de400fc
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2015-02-21 09:45:08 -0500

    unit tests

--- code/demo/test/demo/core_test.clj
+++ code/demo/test/demo/core_test.clj
@@ -1,7 +1,26 @@
 (ns demo.core-test
   (:require [clojure.test :refer :all]
+            [clojure.core.typed :as t :refer [check-ns]]
             [demo.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn bad-check-ns? [ns]
+  (boolean
+    (seq (:delayed-errors 
+           (t/check-ns-info 'demo.eg4)))))
+
+(deftest type-examples
+  (testing "simple occurrence typing"
+    (is (check-ns 'demo.eg1)))
+  (testing "simple let-aliasing"
+    (is (check-ns 'demo.eg2)))
+  (testing "let-aliasing with paths"
+    (is (check-ns 'demo.eg3)))
+  (testing "bad merge"
+    (is (bad-check-ns? 'demo.eg4)))
+  (testing "HMap multimethod"
+    (is (check-ns 'demo.eg5)))
+  (testing "HVec multimethod"
+    (is (check-ns 'demo.eg6)))
+  (testing "double dispatch multimethod"
+    (is (check-ns 'demo.eg7)))
+  )
